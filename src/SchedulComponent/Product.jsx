8d7362293f503a7aa7a89d8b8572f670c298333a
rubencommisso <commissoruben@gmail.com>
2025-03-14 17:05:47 +0100

    add browser router

---
+++ src/SchedulComponent/Product.jsx
@@ -0,0 +1,21 @@
+const Product = () => {
+    return(
+        <div class="container">
+      <h1></h1>
+      <h2></h2>
+      <div class="phone-container">
+        <span></span>
+        <span></span>
+      </div>
+      <div class="phone-model-container">
+        <span></span>
+        <span></span>
+      </div>
+      <div class="card-component">
+        <span></span>
+      </div>
+    </div>
+    )
+}
+
+export default Product

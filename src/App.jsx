7a7bae28f067f53cf8cb67ae89be784a6251a2a8
rubencommisso <commissoruben@gmail.com>
2025-03-17 09:50:26 +0100

    Add mod route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Carousell from './components/Carousell'
+import { Routes, Route } from 'react-router-dom'
 import Cart from './pages/Cart'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Routes>
-     <Route  path='/carousell' element={<Carousell />} />  
 
-      </Routes>
-      
-      <Routes>
-        <Route  path='/cart' element={<Cart/>}  />
+       
+        <Route path='/cart' element={<Cart />} />
 
       </Routes>
     </div>

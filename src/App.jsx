9b1a1c03770645e586eebfe085f6d6cdfb04cf55
marinademinescu <demi.marina@yahoo.it>
2025-04-07 15:26:53 +0200

    Modificato la pagina di: Footer.jsx e App.jsx, aggiunto 3 pagine nuove: TermosofService.jsx, PrivacyPolicy e CookiePolicy.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,35 @@ import Homepage from "./pages/Homepage";
 import Ring from "./pages/Ring";
 import KitPulizia from "./pages/KitPulizia";
 import Cover from "./pages/Cover";
+import CookiePolicy from "./pages/CookiePolicy";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsofService";
 
 
 
 const App = () => {
   return (
-      <Routes>
-        <Route element={<Layout />}> {/* Layout Route */}
-          <Route path="/" element={<Homepage/>} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/sustainability"  element={<Sustainability />} />  
-          <Route path="/about-us"  element={<AboutUs />} />  
-          <Route path="/sustainability/:productId" element={<Sustainability />} />
-          <Route path="/contact-us"  element={<ContactPage />} /> 
-          <Route path="/ring"  element={<Ring />} /> 
-          <Route path="/pellicole" element={<Pellicole />} />
-          <Route path="/kit-pulizia" element={<KitPulizia />} />
-          <Route path="/showdetail/:productId" element={<ShowDetail />} /> 
-          <Route path="/set-accessori" element={<Product />} />
-          <Route path="/showdetail" element={<ShowDetail />} />
-          <Route path="/cover" element={<Cover/>} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route element={<Layout />}> {/* Layout Route */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/sustainability" element={<Sustainability />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/sustainability/:productId" element={<Sustainability />} />
+        <Route path="/contact-us" element={<ContactPage />} />
+        <Route path="/ring" element={<Ring />} />
+        <Route path="/pellicole" element={<Pellicole />} />
+        <Route path="/kit-pulizia" element={<KitPulizia />} />
+        <Route path="/showdetail/:productId" element={<ShowDetail />} />
+        <Route path="/set-accessori" element={<Product />} />
+        <Route path="/showdetail" element={<ShowDetail />} />
+        <Route path="/cover" element={<Cover />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className='max-w-5xl mx-auto flex flex-col justify-center px-10 sm:px-6 md:px-10 lg:px-8 mt-10 mb-20 text-xl'>
+       <h1 className='text-4xl text-start font-semibold mb-2'>Privacy Policy</h1>
+      <p>
+        La tua privacy è importante per noi. Questa politica sulla privacy spiega come raccogliamo, utilizziamo e proteggiamo i tuoi dati personali quando utilizzi il nostro sito web.
+      </p>
+      <h2>Informazioni che raccogliamo</h2>
+      <p>
+        Raccogliamo informazioni personali quando ti registri sul nostro sito, effettui un acquisto, o ci contatti tramite i moduli di contatto. Le informazioni possono includere nome, indirizzo email, indirizzo di spedizione, e dati di pagamento.
+      </p>
+      <h2>Come utilizziamo le tue informazioni</h2>
+      <p>
+        Utilizziamo le tue informazioni per processare ordini, rispondere alle tue richieste, migliorare il nostro sito, e inviare comunicazioni relative ai nostri servizi. Non vendiamo né condividiamo le tue informazioni con terzi senza il tuo consenso.
+      </p>
+      <h2>Protezione dei tuoi dati</h2>
+      <p>
+        Adottiamo misure di sicurezza per proteggere i tuoi dati da accessi non autorizzati, alterazione o divulgazione. Utilizziamo tecnologie di cifratura e sicurezze fisiche per garantire la protezione dei dati.
+      </p>
+      <h2>Diritti dell'utente</h2>
+      <p>
+        Hai il diritto di accedere, correggere, o eliminare i tuoi dati personali. Puoi anche opporti al trattamento dei tuoi dati per fini di marketing diretto.
+      </p>
+      <h2>Modifiche alla Privacy Policy</h2>
+      <p>
+        Ci riserviamo il diritto di aggiornare questa Privacy Policy. Ti informeremo di eventuali modifiche tramite un avviso sul nostro sito.
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
